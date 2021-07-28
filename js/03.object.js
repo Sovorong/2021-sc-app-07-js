879146583b8c34ca417bc16450281b3eca3f75c7
booldook <booldook@gmail.com>
2021-07-28 16:42:33 +0900

    ...

--- js/03.object.js
+++ js/03.object.js
@@ -5,31 +5,31 @@ Object: 객체
 function a() {
 
 }
-a();
+a()
 
 함수 표현식
 var a = function() {
 
 }
-a();
+a()
 */
 
 // 지역변수
 function aa() {
-	var bb = 10; // 지역변수
-	console.log(bb);
+	var bb = 10 // 지역변수
+	console.log(bb)
 }
 aa();
-// console.log(bb); // 에러남 못찾음
+// console.log(bb) // 에러남 못찾음
 
 
 // IIFE(즉시실행함수)
 (function () {
-	console.log('B 실행함');
-})();
+	console.log('B 실행함')
+})()
 
-var c = function() {
-	console.log('C 실행함');
+var c = function () {
+	console.log('C 실행함')
 }
 c();
 
@@ -39,15 +39,16 @@ Hoisting(끌어올리다)
 함수선언문은 Hoisting의 대상이다
 함수표현식은 Hoisting의 대상이 아니다. 왜냐 변수는 호이스팅 대상이 아니다.
 */
-console.log("===========");
-d();
+console.log("===========")
+d()
+
 function d() {
-	console.log('D 실행함');
+	console.log('D 실행함')
 }
 
-// e(); // 실행안되고 에러남
+// e() // 실행안되고 에러남
 var e = function () {
-	console.log('E 실행함');
+	console.log('E 실행함')
 }
 
 
@@ -55,31 +56,31 @@ var e = function () {
 - 변수를 깊이 들어가 보자 
 1. 변수에는 모든 것을 넣을 수 있다.
 */
-var a;
-console.log(a);	//undefined
+var a
+console.log(a) //undefined
 
-var a = 10;
-console.log(a);	//10
+var a = 10
+console.log(a) //10
 
-a = "A";
-console.log(a);	//A
+a = "A"
+console.log(a) //A
 
-a = function() { 
-	console.log('a()를 실행함') 
-};
-console.log(a);	// function() {}
-a();
+a = function () {
+	console.log('a()를 실행함')
+}
+console.log(a) // function() {}
+a()
 
 // 그래서 변수가 객체(Object)가 될 수 있다.
 // Human
-console.log('=================');
+console.log('=================')
 var hong = {
 	name: '홍길동',
 	age: 20,
 	weight: 80,
 	height: 180,
 	run: function () {
-		console.log(this.name + '이 뜁니다.');
+		console.log(this.name + '이 뜁니다.')
 	}
 }
 var hong2 = {
@@ -88,17 +89,18 @@ var hong2 = {
 	weight: 80,
 	height: 180,
 	run: function () {
-		console.log(this.name + '이 뜁니다.');
+		console.log(this.name + '이 뜁니다.')
 	}
 }
-console.log(hong.name);
-console.log(hong.age);
-console.log(hong.weight);
-console.log(hong.height);
-hong.run();
+console.log(hong.name)
+console.log(hong.age)
+console.log(hong.weight)
+console.log(hong.height)
+hong.run()
+hong2.run()
 
-console.log(hong);
-console.log(console);
+console.log(hong)
+console.log(console)
 
 var korea = {
 	human: hong
@@ -112,4 +114,5 @@ var universe = {
 	star: earth
 }
 
-console.log(universe);
+console.log(universe)
+console.log(window)
